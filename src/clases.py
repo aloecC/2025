7c7a867393d3ae2a00908f8655c557b282f57a5d
Дарья Щербакова <daraaloec29@gmail.com>
2025-09-23 21:05:30 +0300

    redact clases.py

--- src/clases.py
+++ src/clases.py
@@ -123,7 +123,7 @@ class Category:
             Category.product_count += 1  # Увеличиваем общее количество продуктов
             print(f"Товар {product.name} добавлен в категорию {self.name}")
         else:
-            raise TypeError(f"Товар {product.name} не добавлен в категорию {self.name}")
+            raise TypeError()
 
     @classmethod
     def get_total_categories(cls):
